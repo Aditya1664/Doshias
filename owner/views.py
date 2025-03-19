28689246e218d07d32907f39aec15482d8315921
Pranav <pranav.doshi221@vit.edu>
2025-03-19 22:39:07 +0530

    code updated

--- owner/views.py
+++ owner/views.py
@@ -37,7 +37,7 @@ def add_product(request):
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('search_product')
+            return redirect('add_product')
     else:
         form = ProductForm()
     return render(request, "add_product.html", {"form": form})
